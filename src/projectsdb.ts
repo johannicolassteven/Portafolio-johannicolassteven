ea4eea3cce23393b2bdd387e8c801f42d82ffa87
johannicolassteven <nicosteveneg900@gmail.com>
2023-07-22 10:44:45 -0500

    users List

--- src/projectsdb.ts
+++ src/projectsdb.ts
@@ -4,13 +4,20 @@ export const projects = [
     url: "https://lana-rosa.netlify.app/",
     img: "https://i.ibb.co/Xx5cv8q/Lana-Rosa-Presentaci-n.png",
     git: "https://github.com/johannicolassteven/LanaRosa",
-    description : ""
+    description: "",
   },
   {
     name: "Quiz Page",
     url: "https://quiz-page-johannicolassteven.netlify.app/",
     img: "https://i.ibb.co/6vxtgNP/Quiz.png",
     git: "https://github.com/johannicolassteven/Quiz_Page",
-    description : "Axios"
+    description: "Axios",
+  },
+  {
+    name: "Users List",
+    url: "https://userslist-johannicolassteven.netlify.app/",
+    img: "https://i.ibb.co/0FQjKgY/users-List.png",
+    git: "https://github.com/johannicolassteven/UsersList",
+    description: "Axios",
   },
 ];
